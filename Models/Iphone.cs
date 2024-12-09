b038f58bf567be8b88ae03e1863d4ff60d445e2b
Uesleib <uesleibb@gmail.com>
2024-12-09 09:18:26 -0300

    Projeto concluido

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei,int memoria): base(numero,modelo,imei,memoria)
+        {
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp){
+                Console.WriteLine($"{nomeApp} instalado no Iphone");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia=new Nokia("123","1","2",4);
+
+Smartphone iphone=new Iphone("321","412323","34545",4);
+
+nokia.InstalarAplicativo("Whatsapp");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+
+iphone.InstalarAplicativo("Whatsapp");
+iphone.Ligar();
+iphone.ReceberLigacao();
